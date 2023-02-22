8d110a35d9cbceeaebc172bd01649b6b12b027ca
F1sher-Man <fischbach.karol96@gmail.com>
2023-02-22 20:23:22 +0100

    Configure lint. Add primeng.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'admin-app'`, () => {
+  it('should have as title "admin-app"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('admin-app');
